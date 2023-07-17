449c2bf6b659ac79dae73dc83fd13a508769e3ac
alvaroachury <alvaroachury@gmail.com>
2023-07-17 15:38:51 -0500

    Update EX_LED_blink.py
    edit time package

--- examples/src/EX_LED_blink.py
+++ examples/src/EX_LED_blink.py
@@ -1,10 +1,10 @@
 import machine
-import timmer
+import time
 
 led = machine.Pin(15, machine.Pin.OUT)
 
 while True:
     led.value(1)
-    timmer.sleep(1)
+    time.sleep(1)
     led.value(0)
-    timmer.sleep(1)
+    time.sleep(1)
